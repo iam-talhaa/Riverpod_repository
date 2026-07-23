9364eba31d28f507e2370bc437531bf29ce9588e
MUHAMMAD TALHA <102948710+iam-talhaa@users.noreply.github.com>
2026-07-23 23:44:52 +0500

    Updated

---
+++ lib/Favitems/MOdels/itemsModel.dart
@@ -0,0 +1,13 @@
+
+
+
+class Itemsmodel {
+final String name;
+final String Descriptions;
+final bool fav;
+
+Itemsmodel({required this.Descriptions,required this.fav,required this.name})
+
+
+Itemsmodel
+}
