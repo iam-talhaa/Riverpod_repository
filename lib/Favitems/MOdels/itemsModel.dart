ab89bbb84a0c306ada61d89fd05d02a5cb84bb24
MUHAMMAD TALHA <102948710+iam-talhaa@users.noreply.github.com>
2026-07-24 23:26:07 +0500

    Copy with Method

--- lib/Favitems/MOdels/itemsModel.dart
+++ lib/Favitems/MOdels/itemsModel.dart
@@ -8,5 +8,16 @@ final bool fav;
 
 Itemsmodel({required this.Descriptions,required this.fav,required this.name})
 
-
+Itemsmodel.copywith({
+  this.Descriptions,
+  this.fav,
+  this.name,
+})  {
+    return Itemsmodel(
+      name: name ?? this.name,
+      Descriptions: Descriptions ?? this.Descriptions,
+      fav: fav ?? this.fav,
+    )   
 }
+
+}   
